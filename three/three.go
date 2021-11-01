db9e31f586d7c5defcc0b02adcf4564e021a9521
Sam Colson <sam.colson@engineerbetter.com>
2021-11-01 15:00:45 +0000

    Interpolate ^ symbol

--- three/three.go
+++ three/three.go
@@ -1,17 +1,18 @@
 package three
 
 func SetCoords(input string) (output [][]int, err error) {
-	var newCoords []int
+	// var newCoords []int
 
 	startCoords := []int{0, 0}
 	output = append(output, startCoords)
 
 	if input == ">" {
 		updateCoords := []int{startCoords[1] + 1, startCoords[0]}
-		newCoords = updateCoords
+		output = append(output, updateCoords)
+	} else if input == "^" {
+		updateCoords := []int{startCoords[0], startCoords[1] + 1}
+		output = append(output, updateCoords)
 	}
-
-	output = append(output, newCoords)
 
 	return output, nil
 }
--- three/three_test.go
+++ three/three_test.go
@@ -9,11 +9,19 @@ import (
 
 var _ = Describe("Three", func() {
 
-	It("Works out one output based on coordinates", func() {
+	It("Works out one output based on coordinate: '>'", func() {
 		coords, err := three.SetCoords(">")
 
 		Expect(err).NotTo(HaveOccurred())
 		Expect(coords[0]).To(Equal([]int{0, 0}))
 		Expect(coords[1]).To(Equal([]int{1, 0}))
+	})
+
+	It("Works out one output based on coordinate: '^'", func() {
+		coords, err := three.SetCoords("^")
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(coords[0]).To(Equal([]int{0, 0}))
+		Expect(coords[1]).To(Equal([]int{0, 1}))
 	})
 })
